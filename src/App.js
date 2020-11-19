fd869625e2aaf2299abd8568a7a239550a18cfc9
Hasan-new <mdhasan.yahoo@gmail.com>
2020-11-19 11:29:04 +0600

    whatwedo done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import WhatWeDo from './Components/WhatWeDo/WhatWeDo';
+import ChoseYourTeam from './Components/WhatWeDo/ChoseYourTeam/ChoseYourTeam';
 
 function App() {
   return (
     <div className="App">
+      
       <WhatWeDo>
-        
+        {/* <h1>What We Do</h1> */}
       </WhatWeDo>
+      <ChoseYourTeam></ChoseYourTeam>
+      
     </div>
   );
 }
